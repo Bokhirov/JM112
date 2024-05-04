379c6b30c2a090d2fb174fd98b058d1dec7ebab8
Бохиров <bokhirov1@gmail.com>
2024-05-04 20:14:10 +0500

    first

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # 1. Операторы больше и меньше в Питоне
 
-sts = int(input("Give me numbers?"))
+# sts = int(input("Give me numbers?"))
+# if sts > 0:
+#     print("True")
+# else:
+#     print("False")
+
+sts = 5
 if sts > 0:
     print("True")
 else:
